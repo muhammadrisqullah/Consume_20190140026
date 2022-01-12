cb688aaacea0278032867082db497a4e8ce4a6d6
muhammadrisqullah <80031539+muhammadrisqullah@users.noreply.github.com>
2022-01-13 00:43:35 +0700

    tabel,searchbar

--- myscripts.js
+++ myscripts.js
@@ -1,31 +1,29 @@
-
-function opendata(){
-    let data = document.getElementById("view");
-    let url ="http://localhost:8080/game/json";
-    
-    $.getJSON(url, function(result){
-        console.log(result)
-        $.each(result, function(a){
-            
-            data.innerHTML += "<br>" +
-            "Id Game :" + result[a].idGame + 
-            ", Nama : " + result[a].nama + 
-            ", Stock game :" + result[a].stockgame + 
-            ", Harga :" + result[a].harga  +
-            "<hr>"   
-        })
-        
-        
-        if (data.style.display === "none") {
-            data.style.display = "block";
-        } 
-        
-        
-        
-
-    })
-    
+function getAll(){
+    const respon = axios.get("http://localhost:8080/game/json")
+    const dr = respon.then(resp => resp.data)
+    return dr
 }
+
+$("#mytable").ready(function () {
+    var data = document.getElementById("mytable")
+    getAll().then(obj => {
+        for (var i = 0; i < obj.length; i++) {
+            const tr = data.insertRow()
+            const td1 = tr.insertCell();
+            const td2 = tr.insertCell();
+            const td3 = tr.insertCell();
+            const td4 = tr.insertCell();
+            const td5 = tr.insertCell();
+            console.log(obj[i])
+            td1.innerHTML = obj[i].idGame
+            td2.innerHTML = obj[i].nama
+            td3.innerHTML = obj[i].stockgame
+            td4.innerHTML = obj[i].harga
+            td5.innerHTML = '<a class="btn btn-success" href="" role="button">Edit Data</a> '+'<a class="btn btn-danger" href="" role="button">Hapus Data</a><br><br>'
+        }
+    }
+    )
+});
 
 
 
